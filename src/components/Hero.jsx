46db43b122ace4a44b9787b2496b39c1594abe1a
Baishali Datta <baishali.datta@blackngreen.com>
2022-12-03 18:17:01 +0530

    images + ui changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
-import Image from "../assets/earth_hex.png";
+import Image1 from "../assets/earth_hex.png";
+import Image2 from "../assets/custom-hex.png";
+import Image3 from "../assets/rainbow-hex.png";
+import GoldLines from "../assets/gold-lines.png";
 
 export default function Hero() {
   return (
     <div className="h-70v w-full bg-hero flex">
+      {/* <img src={GoldLines} /> */}
       <Title />
       <Grid />
     </div>
@@ -18,14 +22,14 @@ const Grid = () => {
     <div className="w-3/5 flex flex-col items-center justify-center overflow-hidden">
       <div className="h-1/2 flex justify-center relative">
         <img
-          src={Image}
+          src={Image1}
           className="absolute h-full -translate-y-[calc(75%+1rem)]"
         />
-        <img src={Image} className="-translate-x-2" />
-        <img src={Image} className="translate-x-2" />
+        <img src={Image1} className="-translate-x-2" />
+        <img src={Image2} className="translate-x-2" />
         <img
-          src={Image}
-          className=" absolute h-full translate-y-[calc(75%+1rem)]"
+          src={Image3}
+          className="absolute h-full translate-y-[calc(75%+1rem)]"
         />
       </div>
     </div>
