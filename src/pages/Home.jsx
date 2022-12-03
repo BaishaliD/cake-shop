655ce7399ad744158c45fba5528a96e7b4e7ecba
Baishali Datta <baishali.datta@blackngreen.com>
2022-12-04 05:14:36 +0530

    home page ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
 import Hero from "../components/Hero";
 import Wave from "../components/Wave";
 import InfoStrip from "../components/InfoStrip";
+import NewLaunch from "../components/NewLaunch";
+import Collections from "../components/Collections";
+import BestSellers from "../components/BestSellers";
+import ReviewBoard from "../components/ReviewBoard";
 
 export default function Home() {
   return (
     <div className="pt-24">
       <Hero />
       {/* <Wave /> */}
-      <InfoStrip />
+      {/* <InfoStrip /> */}
+      <div className="w-full flex">
+        <NewLaunch />
+        <InfoStrip />
+      </div>
+      <BestSellers />
+      <Collections />
+      <ReviewBoard />
     </div>
   );
 }
