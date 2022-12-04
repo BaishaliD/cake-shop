fd5d8091bbb7e1ccc7909dc74a491caa5e403f64
BaishaliD <baishali95920@gmail.com>
2022-12-04 17:57:30 +0530

    ant ui + product page carousel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { createBrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 import AboutUs from "./pages/AboutUs";
 import NavBar from "./components/NavBar";
 import ProductStrip from "./components/ProductStrip";
@@ -19,6 +20,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/product" element={<Product />} />
         <Route path="/faq" element={<div>FAQ Page </div>} />
       </Routes>
     </div>
---
+++ src/pages/Product.jsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+import Carousel from "../components/Carousel";
+
+export default function Product() {
+  return (
+    <div className="pt-24">
+      <Button type="primary">Button</Button>
+      <Carousel />
+    </div>
+  );
+}
