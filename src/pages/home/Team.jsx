5db2fb480fb3c2b1ea6418bc4b825213c5770d3b
BaishaliD <baishali95920@gmail.com>
2022-12-19 20:05:43 +0530

    homepage ui compoennts

--- src/pages/home/Team.jsx
+++ src/pages/home/Team.jsx
@@ -1,23 +1,25 @@
-import Baking from "../../assets/baking.gif";
+import Man1 from "../../assets/team/man.png";
+import Man2 from "../../assets/team/man-2.png";
+import Woman from "../../assets/team/woman.png";
 
 const bakersList = [
   {
     id: 1,
     name: "John Travolta",
-    des: "Head Chef",
-    image: "",
+    des: "Chef",
+    image: Man1,
   },
   {
     id: 2,
-    name: "John Travolta",
+    name: "Monica",
     des: "Head Chef",
-    image: "",
+    image: Woman,
   },
   {
     id: 3,
-    name: "John Travolta",
-    des: "Head Chef",
-    image: "",
+    name: "Salman Khan",
+    des: "Chef",
+    image: Man2,
   },
 ];
 
@@ -26,8 +28,8 @@ export default function Team() {
     <div className="relative w-full h-96 text-white parallax3">
       {/* <img src={Baking} className="h-full w-full object-cover" /> */}
       <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
-        <div className="text-3xl p-2">
-          Meet our starts who bake the prefect cake for you every time!
+        <div className="text-3xl p-2 bg-accent2 mb-4">
+          Meet our stars who bake the prefect cake for you every time!
         </div>
         <div className="flex">
           {bakersList.map((item) => (
@@ -46,8 +48,12 @@ export default function Team() {
 
 const Item = ({ name, image, des }) => {
   return (
-    <div className="flex flex-col justify-center items-center m-4">
-      <img src={""} className="h-40 w-40 rounded-full bg-white" />
+    <div className="flex flex-col justify-center items-center mx-8">
+      <img
+        src={image}
+        className="h-40 w-40 rounded-full bg-secondary2 border"
+        style={{ borderColor: "#251a1a", borderWidth: "4px" }}
+      />
       <h3 className="my-2">{name}</h3>
       <p>{des}</p>
     </div>
