16c496c98be644f030f502d9993c86ab7900c0aa
BaishaliD <baishali95920@gmail.com>
2023-05-30 03:36:01 +0530

    wishlist UI - handle edge cases

--- src/pages/wishlist/Wishlist.main.jsx
+++ src/pages/wishlist/Wishlist.main.jsx
@@ -1,29 +1,93 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { fetchRandomList } from "../../../firebase";
 import ProductCard from "../../components/ProductCard";
+import { fetchWishlist, removeFromWishlist } from "../../../firebase";
+import { Spin } from "antd";
 
 export default function Wishlist() {
   const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetchRandomList(10)
-      .then((res) => setList(res))
-      .catch((e) => console.log("Wishlist fetch API error ", e));
+    fetchWishlist()
+      .then((res) => {
+        if (res && res.length > 0) {
+          setList(res);
+          setError(null);
+        } else {
+          setError("NO_WISHLIST");
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err === "NO_LOGGED_IN_USER") {
+          setError("NO_LOGGED_IN_USER");
+        } else {
+          setError("SOMETHING_WENT_WRONG");
+        }
+      });
   }, []);
 
-  const removeFromWishlist = (id) => {
-    console.log("REMOVE FROM WISHLIST : ", id);
-    let _index = list.findIndex((item) => item.id === id);
-    list.splice(_index, 1);
-    console.log("LIST :: ", list);
-    setList(list);
+  const handleRemoveFromWishlist = (id) => {
+    removeFromWishlist(id)
+      .then((res) => {
+        if (res && res.length > 0) {
+          setList(res);
+          setError(null);
+        } else {
+          setError("NO_WISHLIST");
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err === "NO_LOGGED_IN_USER") {
+          setError("NO_LOGGED_IN_USER");
+        } else {
+          setError("SOMETHING_WENT_WRONG");
+        }
+      });
   };
+
+  if (error) {
+    return (
+      <div className="min-h-screen flex justify-center items-center text-2xl text-gray-500">
+        {error === "NO_WISHLIST" ? (
+          <div className="text-center">
+            <div>You have not wishlisted anything yet</div>
+            <a href="/products">
+              <div className="underline text-accent1">Add to wishlist</div>
+            </a>
+          </div>
+        ) : error === "NO_LOGGED_IN_USER" ? (
+          <div className="text-center">
+            <div>Log in to view your Wishlist</div>
+            <a href="/login">
+              <div className="underline text-accent1">Login</div>
+            </a>
+          </div>
+        ) : error === "SOMETHING_WENT_WRONG" ? (
+          "Oops, seems like something is not right. We are working on fixing it."
+        ) : null}
+      </div>
+    );
+  }
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <Spin size="large" />
+      </div>
+    );
+  }
 
   return (
     <div className="pt-24 bg-secondary2 flex flex-col items-center">
       <h3 className="mt-8 text-accent1">
-        My Wishlist ({list ? list.length : 0} items)
+        <span> My Wishlist</span>{" "}
+        {list && list.length > 0 && (
+          <span className="font-thin">({list.length} items)</span>
+        )}
       </h3>
       <div className="w-full flex justify-center flex-wrap">
         {list && list.length > 0 ? (
@@ -32,14 +96,13 @@ export default function Wishlist() {
               key={item.id}
               id={item.id}
               name={item.name}
-              image={item.images && item.images[0]}
-              price={item.minPrice}
+              image={item.image}
+              price={item.price}
               discountedPrice={item.discountedPrice}
               rating={item.rating}
               wishlist={true}
+              handleRemoveFromWishlist={handleRemoveFromWishlist}
               eggless={item.eggless}
-              buttonText="Move to Cart"
-              removeFromWishlist={removeFromWishlist}
             />
           ))
         ) : (
